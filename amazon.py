72b36766638f8d1ad6d403f21cc2795f5344f838
Afnan-Navaz <afnannavaz@gmail.com>
2019-01-13 21:44:05 +0530

    base scraper

--- amazon.py
+++ amazon.py
@@ -3,8 +3,8 @@ import requests
 
 link=input("ASIN: ")
 headers = {'User-agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36'}
-url=requests.get('https://www.amazon.in/dp/'+link+'?tag=YOURASSOCIATEID').text
-soup=BeautifulSoup(url,'lxml')
+url=requests.get('https://www.amazon.in/dp/'+link+'?tag=YOURASSOCIATEID', headers=headers).text
+soup=BeautifulSoup(url, 'lxml')
 if soup.find('h1', id='title').span['id'] == "productTitle":
 	xid='productTitle'
 else:
@@ -17,4 +17,9 @@ print("RATING = "+soup.find('span', id='acrPopover')['title'])
 price=soup.find('div', id='twister').find_all('div', class_='top-level')
 for i in price:
 	print(i.find('span', class_='a-color-base').text+" = "+i.find('span', class_='a-color-price').text)
+prodet=soup.find('div', id='detail_bullets_id').ul.find_all('li')
+for pro in prodet:
+	prod=str(pro.text).split(" ")
+	if len(prod)==2:
+		print(prod[0]+" = "+prod[1])
 
